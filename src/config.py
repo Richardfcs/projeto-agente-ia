9b29ddc5d386fa37aa9ebeb4171ab34192cdc4a3
Richardfcs <richardfernandodecastrodasilva@gmail.com>
2025-09-23 18:19:06 -0300

    config.py upd 0.0.3

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# Arquivo: src/config.py
+
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+class Config:
+    """Classe de configuração que busca as variáveis de ambiente."""
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_URI = os.environ.get('MONGO_URI')
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+    CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
