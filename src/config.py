56aba1a5342486724e94e8e159dba0bc644ea815
Richardfcs <richardfernandodecastrodasilva@gmail.com>
2025-10-29 16:00:20 -0300

    token Auth 1.7.6

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@
 
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -11,3 +12,4 @@ class Config:
     MONGO_URI = os.environ.get('MONGO_URI')
     MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME')
     GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
